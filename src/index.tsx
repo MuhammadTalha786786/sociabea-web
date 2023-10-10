cafeb2b084fc2f65a9ca82f634a19d5ebacdc7c2
Muhammad Talha Bin Tufail <82726810+MuhammadTalha786786@users.noreply.github.com>
2023-10-10 14:41:33 +0500

    Revert "add post card"

--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store from './redux/Store';
-import { persistor } from './redux/Store';
-
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
